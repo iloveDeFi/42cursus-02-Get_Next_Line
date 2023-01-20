89790442c878bb4fb15e1ecbe3b72a64936f837f
Baptiste Bessard <bessardbusiness@gmail.com>
2023-01-20 18:15:50 +0100

    edit

--- get_next_line.h
+++ get_next_line.h
@@ -12,23 +12,18 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 42
 # endif
-
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
 # include <fcntl.h>
 
-int			ft_strlen(const char *str);
-char		*ft_strchr(char *str, int c);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*get_next_line(int fd);
-char		*ft_fill_stash(int fd, char *stash);
-char		*ft_extract_line(char *stash);
-char		*ft_new_stash(char *stash);
+char    *get_next_line(int fd);
+char    *ft_strdup(char *s);
+size_t    ft_strlen(char *s);
+char    *ft_substr(char *s, unsigned int start, size_t len);
+char    *ft_strjoin(char *s1, char *s2);
+void    fill_str(char *res, char *s1, char *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,61 +12,80 @@
 
 #include "get_next_line.h"
 
-int	ft_strlen(const char *str)
+char	*ft_strdup(char *s1)
+{
+	char			*dest;
+	unsigned int	i;
+
+	dest = (char *) malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+size_t	ft_strlen(char *s)
 {
 	int	i;
 
 	i = 0;
-	if (str[i] == '\0')
-		return (0);
-	while (str[i] != '\0')
-			i++;
+	while (s[i])
+		i++;
 	return (i);
 }
 
-char	*ft_strchr(char *str, int c)
+char	*ft_substr(char *s, unsigned int start, size_t len)
 {
-	int	i;
+	size_t	i;
+	char	*str;
 
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (malloc(1));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
 	i = 0;
-	while (str[i] != '\0')
+	while (i < len)
 	{
-		if (str[i] == (char)c)
-			return ((char *)str + i);
+		str[i] = s[start + i];
 		i++;
 	}
-	if (str[i] == (char)c)
-		return ((char *)str);
-	if ((char)c == '\0')
-		return ((char *)str);
-	return (0);
+	str[i] = 0;
+	return (str);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	char	*result;
-	int		i;
-	int		j;
-	int		len;
+	char			*res;
+
+	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	fill_str(res, s1, s2);
+	return (res);
+}
+
+void	fill_str(char *res, char *s1, char *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
 	j = 0;
-	len = (ft_strlen(s1) + ft_strlen(s2));
-	result = malloc(sizeof(char) * (len + 1));
-	if (result == NULL)
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		result[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		result[i] = s2[j];
-		i++;
-		j++;
-	}
-	result[i] = '\0';
-	free ((void *)(s1));
-	return (result);
+	while (s1[j])
+		res[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
 }
