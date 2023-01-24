5d515ea197cc73e74ab53d2a7644fde0439cfd2d
Baptiste Bessard <bessardbusiness@gmail.com>
2023-01-24 15:20:08 +0100

    bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,62 +6,82 @@
 /*   By: bbessard <bbessard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 18:45:20 by bbessard          #+#    #+#             */
-/*   Updated: 2023/01/24 15:01:44 by bbessard         ###   ########.fr       */
+/*   Updated: 2023/01/24 15:19:35 by bbessard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "stdio.h"
 
-static char	*_fill_line_buffer(int fd, char *left_c, char *buffer);
-static char	*_set_line(char *line);
-static char	*ft_strchr(char *s, int c);
-
+static char		*_fill_line_buffer(int fd, char *left_c, char *buffer);
+static char		*_set_line(char *line);
+static char		*ft_strchr(char *s, int c);
 
 char	*get_next_line(int fd)
 {
-	static char	*left_c;
-	char	*line;
-    char	*buffer;
-    
-    buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
-    {
-		free(left_c);
+	/* There's only a minimal difference to make the bonus
+	 * work
+	 * It's basically transforming our static char * variable
+	 * to an array of char *
+	 * as you can see I set the number of elements of the array to 
+	 * the constant MAX_FD (see get_next_line.h to see what it is)
+	 */
+	static char	*left_c[MAX_FD];
+	char		*line;
+	char		*buffer;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		/* as we changed our static char * to an array
+		 * we have to specify wich index we wanna work on
+		 * the easier thing to do is to set it to fd
+		 */
+		free(left_c[fd]);
 		free(buffer);
-		left_c = NULL;
+		left_c[fd] = NULL;
 		buffer = NULL;
+		return (0);
+	}
+	if (!buffer)
 		return (NULL);
-    }
-    if (!buffer)
+	/* again here, we want to store the left characters in 
+	 * the array at the index of the fd, so if we have another fd
+	 * we won't be overwriting what was left from the other fd
+	 */
+	line = _fill_line_buffer(fd, left_c[fd], buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
 		return (NULL);
-    line = _fill_line_buffer(fd, left_c, buffer);
-    free(buffer);
-    buffer = NULL;
-    if (!line)
-		return (NULL);
-	left_c = _set_line(line);
+	/* and here again, we have to switch from left_c to left_c[fd]
+	 * that's the last thing we have to change, all the other place
+	 * we use left_c (in all other functions), we use it as a string
+	 * therefore, because we are passing left_c[fd] as parameter
+	 * we basically are passing strings as parameter
+	 * so no problem there, and nothing to change.
+	 */
+	left_c[fd] = _set_line(line);
 	return (line);
 }
 
-static char *_set_line(char *line_buffer)
+static char	*_set_line(char *line_buffer)
 {
-    char    *left_c;
-    ssize_t    i;
-    
-    i = 0;
-    while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
-        i++;
-    if (line_buffer[i] == 0 || line_buffer[1] == 0)
-        return (NULL);
-    left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
-    if (*left_c == 0)
-    {
-        free(left_c);
-        left_c = NULL;
-    }
-    line_buffer[i + 1] = 0;
-    return (left_c);
+	char	*left_c;
+	ssize_t	i;
+
+	i = 0;
+	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+		i++;
+	if (line_buffer[i] == 0 || line_buffer[1] == 0)
+		return (0);
+	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
+	if (*left_c == 0)
+	{
+		free(left_c);
+		left_c = NULL;
+	}
+	line_buffer[i + 1] = 0;
+	return (left_c);
 }
 
 static char	*_fill_line_buffer(int fd, char *left_c, char *buffer)
@@ -76,7 +96,7 @@ static char	*_fill_line_buffer(int fd, char *left_c, char *buffer)
 		if (b_read == -1)
 		{
 			free(left_c);
-			return (NULL);
+			return (0);
 		}
 		else if (b_read == 0)
 			break ;
